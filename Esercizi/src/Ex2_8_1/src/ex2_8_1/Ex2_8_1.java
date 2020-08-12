16ebcf192e8bf63e068ac5f0ee72047435f8908c
Alessandro Giaquinto <a.giaquinto8@studenti.unisa.it>
2020-08-12 23:53:13 +0200

    2.8.1

---
+++ Esercizi/src/Ex2_8_1/src/ex2_8_1/Ex2_8_1.java
@@ -0,0 +1,40 @@
+/*
+Copyright (C) 2020  Alessandro Giaquinto agiaquinto87@gmail.com
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+package ex2_8_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alessandrogiaquinto
+ */
+public class Ex2_8_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner tastiera = new Scanner(System.in);
+        
+        System.out.println("Scrivi la temperatura in gradi Fahrenheit:");
+        
+        int fahre = tastiera.nextInt();
+        
+        double celsius = 5 * (fahre - 32) / 9;
+        
+        System.out.printf(fahre + " gradi Fahreneit corrispondono a %.1f gradi Celsius\n", celsius);
+    }
+    
+}
