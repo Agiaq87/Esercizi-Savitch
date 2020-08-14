db3185fd0a1b73feb10d8310e28e5072d30cc6c8
Alessandro Giaquinto <a.giaquinto8@studenti.unisa.it>
2020-08-14 08:26:18 +0200

    4.4.12

---
+++ Esercizi/src/Ex4_4_12/src/ex4_4_12/Ex4_4_12.java
@@ -0,0 +1,36 @@
+/*
+Copyright (C) 2020  Alessandro Giaquinto agiaquinto87@gmail.com
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+package ex4_4_12;
+
+/**
+ *
+ * @author alessandrogiaquinto
+ */
+public class Ex4_4_12 {
+    
+    public enum Month {JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC};
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for (Month m : Month.values()){
+            System.out.println(m);
+        }
+    }
+    
+}
+
+
