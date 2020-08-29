5fe3a95006f71e5b7c9a4fbec3d6cb717d2180d2
Alessandro Giaquinto <a.giaquinto8@studenti.unisa.it>
2020-08-29 22:16:05 +0200

    Ex6_6_4

--- Esercizi/src/Ex6_6_4/src/ex6_6_4/Ex6_6_4.java
+++ Esercizi/src/Ex6_6_4/src/ex6_6_4/Ex6_6_4.java
@@ -14,6 +14,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 package ex6_6_4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alessandrogiaquinto
@@ -24,7 +26,51 @@ public class Ex6_6_4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        var telephone = new int[10];
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Inserire il numero di telefono:");
+        String temp = s.nextLine();
+        
+        for (char c: temp.toCharArray()) {
+            switch (c) {
+                case '1' -> {
+                    telephone[1]++;
+                }
+                case '2' -> {
+                    telephone[2]++;
+                }
+                case '3' -> {
+                    telephone[3]++;
+                }
+                case '4' -> {
+                    telephone[4]++;
+                }
+                case '5' -> {
+                    telephone[5]++;
+                }
+                case '6' -> {
+                    telephone[6]++;
+                }
+                case '7' -> {
+                    telephone[7]++;
+                }
+                case '8' -> {
+                    telephone[8]++;
+                }
+                case '9' -> {
+                    telephone[9]++;
+                }
+                default -> {
+                    telephone[0]++;
+                }
+            }
+        }
+        
+        for (int i = 0; i < telephone.length; i++) {
+            System.out.println("[" + i + "] -> " + telephone[i]);
+        }
+        
     }
     
 }
